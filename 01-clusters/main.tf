cf88e2889b461b9ea81884301e8203a5505d6a14
ccie7599 <bapley@akamai.com>
2022-11-21 13:35:03 -0500

    Update main.tf

--- 01-clusters/main.tf
+++ 01-clusters/main.tf
@@ -10,6 +10,30 @@ resource "local_file" "kubeconfig_cluster_manager" {
   content  = module.cluster_manager.kubeconfig
 }
 
+module "jp" {
+  source = "../modules/cluster"
+
+  name   = "jp"
+  region = "ap-northeast"
+}
+
+resource "local_file" "kubeconfig_jp" {
+  filename = "../kubeconfig-jp"
+  content  = module.jp.kubeconfig
+} 
+
+module "jp" {
+  source = "../modules/cluster"
+
+  name   = "jp"
+  region = "ap-northeast"
+}
+
+resource "local_file" "kubeconfig_jp" {
+  filename = "../kubeconfig-jp"
+  content  = module.jp.kubeconfig
+}
+  
 module "ap" {
   source = "../modules/cluster"
 
@@ -26,7 +50,7 @@ module "us" {
   source = "../modules/cluster"
 
   name   = "us"
-  region = "us-west"
+  region = "us-southeast"
 }
 
 resource "local_file" "kubeconfig_us" {
@@ -34,14 +58,49 @@ resource "local_file" "kubeconfig_us" {
   content  = module.us.kubeconfig
 }
 
+module "uk" {
+  source = "../modules/cluster"
+
+  name   = "uk"
+  region = "eu-west"
+}
+
+resource "local_file" "kubeconfig_uk" {
+  filename = "../kubeconfig-uk"
+  content  = module.uk.kubeconfig
+}
 module "eu" {
   source = "../modules/cluster"
 
   name   = "eu"
-  region = "eu-west"
+  region = "eu-central"
+}
+
+resource "local_file" "kubeconfig_eu" {
+  filename = "../kubeconfig-eu"
+  content  = module.uk.kubeconfig
+}
+  
+module "eu" {
+  source = "../modules/cluster"
+
+  name   = "eu"
+  region = "eu-central"
 }
 
 resource "local_file" "kubeconfig_eu" {
   filename = "../kubeconfig-eu"
   content  = module.eu.kubeconfig
+}
+    
+module "sg" {
+  source = "../modules/cluster"
+
+  name   = "sg"
+  region = "ap-south"
+}
+
+resource "local_file" "kubeconfig_sg" {
+  filename = "../kubeconfig-sg"
+  content  = module.sg.kubeconfig
 }
