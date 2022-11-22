cceec691bef927a4bf7bcf95b4348baab90c27b2
Brian Apley <brian@apley.net>
2022-11-21 20:13:49 -0500

    Added sample deployment and service yamls

--- 05-dashboards/main.tf
+++ 05-dashboards/main.tf
@@ -1,12 +1,12 @@
-module "worker_ap" {
+module "worker_au" {
   source = "../modules/dashboard"
 
-  cluster_name    = "ap"
-  kubeconfig_path = abspath("../kubeconfig-ap")
+  cluster_name    = "au"
+  kubeconfig_path = abspath("../kubeconfig-au")
 }
 
-output "kiali_token_ap" {
-  value = module.worker_ap.token
+output "kiali_token_au" {
+  value = module.worker_au.token
 }
 
 module "worker_us" {
@@ -29,4 +29,48 @@ module "worker_eu" {
 
 output "kiali_token_eu" {
   value = module.worker_eu.token
+}
+
+module "worker_uk" {
+  source = "../modules/dashboard"
+
+  cluster_name    = "uk"
+  kubeconfig_path = abspath("../kubeconfig-uk")
+}
+
+output "kiali_token_uk" {
+  value = module.worker_uk.token
+}
+
+module "worker_jp" {
+  source = "../modules/dashboard"
+
+  cluster_name    = "jp"
+  kubeconfig_path = abspath("../kubeconfig-jp")
+}
+
+output "kiali_token_jp" {
+  value = module.worker_jp.token
+}
+
+module "worker_in" {
+  source = "../modules/dashboard"
+
+  cluster_name    = "in"
+  kubeconfig_path = abspath("../kubeconfig-in")
+}
+
+output "kiali_token_in" {
+  value = module.worker_in.token
+}
+
+module "worker_sg" {
+  source = "../modules/dashboard"
+
+  cluster_name    = "sg"
+  kubeconfig_path = abspath("../kubeconfig-sg")
+}
+
+output "kiali_token_sg" {
+  value = module.worker_sg.token
 }
