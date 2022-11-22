e52ccc893de8c6fae431f04821fb7c7bd2c6b5b5
ccie7599 <bapley@akamai.com>
2022-11-21 21:18:12 -0500

    Update main.tf

--- 04-discovery/main.tf
+++ 04-discovery/main.tf
@@ -1,4 +1,4 @@
-module "worker_ap" {
+module "worker_au" {
   source = "../modules/discovery"
 
   cluster_name    = "au"
@@ -40,5 +40,65 @@ module "worker_eu" {
     "sg" = { cluster_name = "sg", kubeconfig_path = abspath("../kubeconfig-sg") }
     "in" = { cluster_name = "in", kubeconfig_path = abspath("../kubeconfig-in") }
     "uk" = { cluster_name = "uk", kubeconfig_path = abspath("../kubeconfig-uk") }
+  }
+}
+
+module "worker_jp" {
+  source = "../modules/discovery"
+
+  cluster_name    = "jp"
+  kubeconfig_path = abspath("../kubeconfig-jp")
+  cluster_discovery = {
+    "au" = { cluster_name = "au", kubeconfig_path = abspath("../kubeconfig-au") }
+    "us" = { cluster_name = "us", kubeconfig_path = abspath("../kubeconfig-us") }
+    "eu" = { cluster_name = "eu", kubeconfig_path = abspath("../kubeconfig-eu") }
+    "sg" = { cluster_name = "sg", kubeconfig_path = abspath("../kubeconfig-sg") }
+    "in" = { cluster_name = "in", kubeconfig_path = abspath("../kubeconfig-in") }
+    "uk" = { cluster_name = "uk", kubeconfig_path = abspath("../kubeconfig-uk") }
+  }
+}
+
+module "worker_sg" {
+  source = "../modules/discovery"
+
+  cluster_name    = "sg"
+  kubeconfig_path = abspath("../kubeconfig-sg")
+  cluster_discovery = {
+    "au" = { cluster_name = "au", kubeconfig_path = abspath("../kubeconfig-au") }
+    "us" = { cluster_name = "us", kubeconfig_path = abspath("../kubeconfig-us") }
+    "eu" = { cluster_name = "eu", kubeconfig_path = abspath("../kubeconfig-eu") }
+    "jp" = { cluster_name = "jp", kubeconfig_path = abspath("../kubeconfig-jp") }
+    "in" = { cluster_name = "in", kubeconfig_path = abspath("../kubeconfig-in") }
+    "uk" = { cluster_name = "uk", kubeconfig_path = abspath("../kubeconfig-uk") }
+  }
+}
+
+module "worker_in" {
+  source = "../modules/discovery"
+
+  cluster_name    = "in"
+  kubeconfig_path = abspath("../kubeconfig-in")
+  cluster_discovery = {
+    "au" = { cluster_name = "au", kubeconfig_path = abspath("../kubeconfig-au") }
+    "us" = { cluster_name = "us", kubeconfig_path = abspath("../kubeconfig-us") }
+    "eu" = { cluster_name = "eu", kubeconfig_path = abspath("../kubeconfig-eu") }
+    "sg" = { cluster_name = "sg", kubeconfig_path = abspath("../kubeconfig-sg") }
+    "jp" = { cluster_name = "jp", kubeconfig_path = abspath("../kubeconfig-jp") }
+    "uk" = { cluster_name = "uk", kubeconfig_path = abspath("../kubeconfig-uk") }
+  }
+}
+
+module "worker_uk" {
+  source = "../modules/discovery"
+
+  cluster_name    = "uk"
+  kubeconfig_path = abspath("../kubeconfig-uk")
+  cluster_discovery = {
+    "au" = { cluster_name = "au", kubeconfig_path = abspath("../kubeconfig-au") }
+    "us" = { cluster_name = "us", kubeconfig_path = abspath("../kubeconfig-us") }
+    "eu" = { cluster_name = "eu", kubeconfig_path = abspath("../kubeconfig-eu") }
+    "sg" = { cluster_name = "sg", kubeconfig_path = abspath("../kubeconfig-sg") }
+    "in" = { cluster_name = "in", kubeconfig_path = abspath("../kubeconfig-in") }
+    "jp" = { cluster_name = "jp", kubeconfig_path = abspath("../kubeconfig-jp") }
   }
 }
